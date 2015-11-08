36824f3fa571f1df3e36c452b650a2fe51e510ad
Antonia Wang <antonia.t.wang@gmail.com>
2015-11-07 23:37:16 -0800

    airbnb link w/ max price created

--- utils.py
+++ utils.py
@@ -1,15 +1,14 @@
 #Encode city for Airbnb URL
-def create_airbnb_url(city, checkin, checkout):
+def create_airbnb_url(city, checkin, checkout, max_price):
     #https://www.airbnb.com/s/Dubai-~-United-Arab-Emirates?guests=2&checkin=01%2F12%2F2016&checkout=01%2F20%2F2016&ss_id=d6heijmr&source=bb
     city_encode = city.replace(',',"-")
     city_encode = city_encode.replace(' ',"-")
 
-    print "city is", city_encode
     checkin_encode = checkin[5:7] + "%2F" + checkin[8:10] + "%2F" + checkin[0:4]
-    print "checkin is", checkin_encode
     checkout_encode = checkout[5:7] + "%2F" + checkout[8:10] + "%2F" + checkout[0:4]
-    print "checkout is", checkout_encode
 
-    airbnb_url = "https://www.airbnb.com/s/" + city_encode + "?guests=2&checkin="+checkin_encode+"&checkout=" + checkout_encode
+    max_daily_rate = max_price / 14
+
+    airbnb_url = "https://www.airbnb.com/s/" + city_encode + "?guests=2&checkin=" + checkin_encode + "&checkout=" + checkout_encode + "&guests=2" + "&price_max=" + str(max_daily_rate)
 
     return airbnb_url
